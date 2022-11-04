5f5e3c40fce9182ebe5aaef9b8cdfbad31c1f5bc
MemoonaMobeen <memoonamobeen618@gmail.com>
2022-11-04 19:32:55 +0500

    fix changes

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">Activity Tracker</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">Activity</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Create Project Log</Link>
           </li>
           <li className="navbar-item">
           <Link to="/user" className="nav-link">Create User</Link>
